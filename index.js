0dfe0f75ec681d4af2904b96d4e136195ba66ad9
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-22 20:51:00 +1000

    commit tree 37195d287f37b7d5ff785950a8f057a0f4b5f0a2

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+// multiplication//
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+let random = Math.floor(Math.random()*10+1);
+let num3 = 14;
+let num4 = 5;
+let mod = num3%num4;
+let numset = (1,2,3,4,5,20);
+let max = Math.max(numset);
